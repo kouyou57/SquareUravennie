c47c2ba3af2398425e2a367a761540b8e098bf13
mary <mzanina35@gmail.com>
2026-08-26 14:23:20 +0300

    case 4: d<0 и чуть-чуть пофиксила кодстайл
    в функции solver добавила проверку на d<0 и новый вариант значения nRoots = 4

--- main_code.c
+++ main_code.c
@@ -1,3 +1,4 @@
+#include <TXLib.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -7,24 +8,25 @@ int Solver(double a, double b, double c, double *x1, double *x2) {
             return (c == 0.0) ? 3 : 0;  /* 3 = infinity */
         }
         else {
-            *x1 = *x2 = -c / b;
+            *x1 = *x2 = - c / b;
             return 1;
         }
     }
 
     else {
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
         if (d == 0.0) {
-            *x1 = *x2 = -b / (2 * a);
+            *x1 = *x2 = - b / (2 * a);
             return 1;
         }
-        else {
+        else if (d > 0) {
             double sqrt_d = sqrt(d);
-            *x1 = (-b + sqrt_d) / (2 * a);
-            *x2 = (-b - sqrt_d) / (2 * a);
+            *x1 = (- b + sqrt_d) / (2 * a);
+            *x2 = (- b - sqrt_d) / (2 * a);
 
             return 2;
         }
+        else return 4; /* 4 - d<0 */
     }
 }
 
@@ -34,28 +36,37 @@ int main() {
     printf("This program solves square equations\n");
     printf("Enter a,b,c through a space (probel)\n");
 
-    double a = 0, b = 0, c = 0; /* коэффициенты */
-    scanf("%lg %lg %lg\n", &a, &b, &c);
-    printf("----------");
+    double a = 0, b = 0, c = 0; /* koefficients */
+    scanf("%lg %lg %lg", &a, &b, &c);
+    printf("----------\n");
 
 
-    double x1 = 0, x2 = 0; /* корни */
-    int nRoots = Solver(a,b,c, &x1, &x2); /*количество решений и корни*/
+    double x1 = 0, x2 = 0; /* solves */
+    int nRoots = Solver(a,b,c, &x1, &x2); /* number of solves */
 
     switch(nRoots) {
-        case 0: printf("no solves!");
-                break;
+        case 0:
+            printf("no solves!");
+            break;
 
-        case 1: printf("x = %lg", x1);
-                break;
+        case 1:
+            printf("x = %lf", x1);
+            break;
 
-        case 2: printf("x1 = %lg, x2 = %lg", x1, x2);
-                break;
+        case 2:
+            printf("x1 = %lf, x2 = %lf", x1, x2);
+            break;
 
-        case 3: printf("infinity solves!");
-                       break;
+        case 3:
+            printf("infinity solves!");
+            break;
 
-        default: printf("error! nRoots = %d", nRoots);
+        case 4:
+            printf("d < 0! zero solves or error");
+
+        default:
+            printf("error! nRoots = %d", nRoots);
+            break
     }
     return 0;
 }
