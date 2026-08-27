286402972ce8289633ad5def57f46214c9ef62ec
mary <mzanina35@gmail.com>
2026-08-27 12:46:45 +0300

    хреновые unit_tests

--- main_code.c
+++ main_code.c
@@ -4,17 +4,16 @@
 
 #define ZERO_BORDER 0.0000009
 
-int Are_Equal(double num1, double num2);
-int Solver(double a, double b, double c, double *x1, double *x2);
-int Test(double a, double b, double c, int nRoots_r, double x1_r, double x2_r);
-void RunAllTests();
-
 struct Refference {
     double a,b,c;
     int nRoots_r;
     double x1_r, x2_r;
-}
-;
+} ;
+
+int Are_Equal(double num1, double num2);
+int Solver(double a, double b, double c, double *x1, double *x2);
+void Test(struct Refference ref);
+void RunAllTests();
 
 /* compares equal or part-equal double. kills warnings. no warnings, no problems */
 int Are_Equal(double num1, double num2) {
@@ -40,7 +39,6 @@ int Solver(double a, double b, double c, double *x1, double *x2) {
     }
     else {
         double d = b * b - 4 * a * c;
-        printf(">> discriminant = %.20lf\n", d);
 
         if (Are_Equal(d,0)) {
             *x1 = *x2 = - b / (2 * a);
@@ -58,37 +56,69 @@ int Solver(double a, double b, double c, double *x1, double *x2) {
 }
 
 /* one test */
-int Test(double a, double b, double c, int nRoots_r, double x1_r, double x2_r) {
+void Test(struct Refference ref) {
+    double a = ref.a;
+    double b = ref.b;
+    double c = ref.c;
+    int nRoots_r = ref.nRoots_r;
+    double x1_r = ref.x1_r;
+    double x2_r = ref.x2_r;
+
+
     double x1 = 0, x2 = 0;
 
     int nRoots = Solver(a,b,c, &x1, &x2);
 
     if (nRoots == nRoots_r) {
-        if ((Are_Equal(x1, x1_r)) && (Are_Equal(x2, x2_r))) {
-            printf("ok");
-            return 0; /* ok */
+        if ((nRoots == 1) || (nRoots == 2)) {
+
+            if ((Are_Equal(x1, x1_r)) && (Are_Equal(x2, x2_r))) {
+                printf("ok\n");
+            }
+            else {
+                printf("------> error!!\n");
+                printf("nRoots ok, nRoots = %d\n", nRoots);
+                printf("expected x1 = %lf, x2 = %lf\n", x1_r, x2_r);
+                printf("got x1 = %lf, x2 = %lf\n", x1, x2);
+            }
         }
         else {
-            printf("------> error!!\n");
-            printf("nRoots ok, nRoots = %d", nRoots);
-            printf("expected x1 = %lf, x2 = %lf", x1_r, x2_r);
-            printf("got x1 = %lf, x2 = %lf", x1, x2);
-            return 1; /* wrong solutions */
+            printf("ok");
         }
     }
     else {
         printf("------> error!!\n");
         printf("expected nRoots = %d\n", nRoots_r);
-        printf("got nRoots = %d", nRoots);
-        return 2; /* wrong number of solutions */
+        printf("got nRoots = %d\n", nRoots);
     }
 }
 
 void RunAllTests() {
-   /* don't forget about this function */
+
+    struct Refference ref0 = {.a = 1, .b = 2, .c = 1, .nRoots_r = 1, .x1_r = -1, .x2_r = -1};
+    struct Refference ref1 = {.a = 1, .b = -2, .c = 1, .nRoots_r = 1, .x1_r = 1, .x2_r = 1};
+    struct Refference ref2 = {.a = 1, .b = 2, .c = 3, .nRoots_r = 4, .x1_r = 0, .x2_r = 0};
+    struct Refference ref3 = {.a = 0, .b = 1, .c = 2, .nRoots_r = 1, .x1_r = -2, .x2_r = -2};
+    struct Refference ref4 = {.a = 1, .b = -4, .c = -21, .nRoots_r = 2, .x1_r = 7, .x2_r = -3};
+    struct Refference ref5 = {.a = 0, .b = 0, .c = 0, .nRoots_r = 3, .x1_r = 0, .x2_r = 0};
+    struct Refference ref6 = {.a = 0, .b = 0, .c = 1, .nRoots_r = 0, .x1_r = 0, .x2_r = 0};
+    struct Refference ref7 = {.a = 0.3333333, .b = 2, .c = 3, .nRoots_r = 1, .x1_r = -3, .x2_r = -3};
+    struct Refference ref8 = {.a = 5, .b = 10, .c = 3, .nRoots_r = 4, .x1_r = 0, .x2_r = 0};
+    struct Refference ref9 = {.a = 0, .b = 0, .c = 15, .nRoots_r = 0, .x1_r = 0, .x2_r = 0};
+
+    struct Refference unit_tests[10] = {ref0, ref1, ref2, ref3, ref4, ref5, ref6, ref7, ref8, ref9} ;
+
+    int number_of_tests = sizeof(unit_tests) ;
+    for (int i=0; i < number_of_tests; i++) {
+        Test(unit_tests[i]);
+    }
+
 }
 
 int main() {
+
+    RunAllTests();
+    printf("\n\n\n\n\n--------------\n");
 
     printf("This program solves square equations\n");
     printf("Enter a,b,c through a space (probel)\n");
