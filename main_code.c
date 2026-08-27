1546d50d361aa4cca6704ea1db0b628ad11a8494
mary <mzanina35@gmail.com>
2026-08-27 14:35:11 +0300

    example

--- main_code.c
+++ main_code.c
@@ -5,14 +5,14 @@
 #define ZERO_BORDER 0.0000009
 
 struct Square_Example {
-    double a,b,c;
-    int nRoots_r;
-    double x1_r, x2_r;
+    double a, b, c;
+    int nRoots;
+    double x1, x2;
 } ;
 
 int Are_Equal(double num1, double num2);
 int Solver(double a, double b, double c, double *x1, double *x2);
-void Test(struct Square_Example ref);
+void Test(struct Square_Example example);
 void RunAllTests();
 
 /* compares equal or part-equal double. kills warnings. no warnings, no problems */
@@ -40,7 +40,7 @@ int Solver(double a, double b, double c, double *x1, double *x2) {
     else {
         double d = b * b - 4 * a * c;
 
-        if (Are_Equal(d,0)) {
+        if (Are_Equal(d, 0)) {
             *x1 = *x2 = - b / (2 * a);
             return 1;
         }
@@ -56,29 +56,29 @@ int Solver(double a, double b, double c, double *x1, double *x2) {
 }
 
 /* one test */
-void Test(struct Square_Example ref) {
-    double a = ref.a;
-    double b = ref.b;
-    double c = ref.c;
-    int nRoots_r = ref.nRoots_r;
-    double x1_r = ref.x1_r;
-    double x2_r = ref.x2_r;
+void Test(struct Square_Example example) {
+    double a = example.a;
+    double b = example.b;
+    double c = example.c;
+    int nRoots_example = example.nRoots;
+    double x1_example = example.x1;
+    double x2_example = example.x2;
 
 
     double x1 = 0, x2 = 0;
 
-    int nRoots = Solver(a,b,c, &x1, &x2);
+    int nRoots = Solver(a, b, c, &x1, &x2);
 
-    if (nRoots == nRoots_r) {
+    if (nRoots == nRoots_example) {
         if ((nRoots == 1) || (nRoots == 2)) {
 
-            if ((Are_Equal(x1, x1_r)) && (Are_Equal(x2, x2_r))) {
+            if ((Are_Equal(x1, x1_example)) && (Are_Equal(x2, x2_example))) {
                 printf("ok\n");
             }
             else {
                 printf("------> error!!\n");
                 printf("nRoots ok, nRoots = %d\n", nRoots);
-                printf("expected x1 = %lf, x2 = %lf\n", x1_r, x2_r);
+                printf("expected x1 = %lf, x2 = %lf\n", x1_example, x2_example);
                 printf("got x1 = %lf, x2 = %lf\n", x1, x2);
             }
         }
@@ -88,25 +88,25 @@ void Test(struct Square_Example ref) {
     }
     else {
         printf("------> error!!\n");
-        printf("expected nRoots = %d\n", nRoots_r);
+        printf("expected nRoots = %d\n", nRoots_example);
         printf("got nRoots = %d\n", nRoots);
     }
 }
 
 void RunAllTests() {
 
-    struct Square_Example ref0 = {.a = 1, .b = 2, .c = 1, .nRoots_r = 1, .x1_r = -1, .x2_r = -1};
-    struct Square_Example ref1 = {.a = 1, .b = -2, .c = 1, .nRoots_r = 1, .x1_r = 1, .x2_r = 1};
-    struct Square_Example ref2 = {.a = 1, .b = 2, .c = 3, .nRoots_r = 4, .x1_r = 0, .x2_r = 0};
-    struct Square_Example ref3 = {.a = 0, .b = 1, .c = 2, .nRoots_r = 1, .x1_r = -2, .x2_r = -2};
-    struct Square_Example ref4 = {.a = 1, .b = -4, .c = -21, .nRoots_r = 2, .x1_r = 7, .x2_r = -3};
-    struct Square_Example ref5 = {.a = 0, .b = 0, .c = 0, .nRoots_r = 3, .x1_r = 0, .x2_r = 0};
-    struct Square_Example ref6 = {.a = 0, .b = 0, .c = 1, .nRoots_r = 0, .x1_r = 0, .x2_r = 0};
-    struct Square_Example ref7 = {.a = 0.3333333, .b = 2, .c = 3, .nRoots_r = 1, .x1_r = -3, .x2_r = -3};
-    struct Square_Example ref8 = {.a = 5, .b = 10, .c = 3, .nRoots_r = 4, .x1_r = 0, .x2_r = 0};
-    struct Square_Example ref9 = {.a = 0, .b = 0, .c = 15, .nRoots_r = 0, .x1_r = 0, .x2_r = 0};
+    struct Square_Example ex0 = {.a = 1, .b = 2, .c = 1, .nRoots = 1, .x1 = -1, .x2 = -1};
+    struct Square_Example ex1 = {.a = 1, .b = -2, .c = 1, .nRoots = 1, .x1 = 1, .x2 = 1};
+    struct Square_Example ex2 = {.a = 1, .b = 2, .c = 3, .nRoots = 4, .x1 = 0, .x2 = 0};
+    struct Square_Example ex3 = {.a = 0, .b = 1, .c = 2, .nRoots = 1, .x1 = -2, .x2 = -2};
+    struct Square_Example ex4 = {.a = 1, .b = -4, .c = -21, .nRoots = 2, .x1 = 7, .x2 = -3};
+    struct Square_Example ex5 = {.a = 0, .b = 0, .c = 0, .nRoots = 3, .x1 = 0, .x2 = 0};
+    struct Square_Example ex6 = {.a = 0, .b = 0, .c = 1, .nRoots = 0, .x1 = 0, .x2 = 0};
+    struct Square_Example ex7 = {.a = 0.3333333, .b = 2, .c = 3, .nRoots = 1, .x1 = -3, .x2 = -3};
+    struct Square_Example ex8 = {.a = 5, .b = 10, .c = 3, .nRoots = 4, .x1 = 0, .x2 = 0};
+    struct Square_Example ex9 = {.a = 0, .b = 0, .c = 15, .nRoots = 0, .x1 = 0, .x2 = 0};
 
-    struct Square_Example unit_tests[10] = {ref0, ref1, ref2, ref3, ref4, ref5, ref6, ref7, ref8, ref9} ;
+    struct Square_Example unit_tests[10] = {ex0, ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9} ;
 
     int number_of_tests = sizeof(unit_tests) ;
     for (int i=0; i < number_of_tests; i++) {
@@ -118,8 +118,8 @@ void RunAllTests() {
 int main() {
 
     RunAllTests();
-    printf("\n\n\n\n\n--------------\n");
 
+    printf("\n\n\n\n\n--------------\n");
     printf("This program solves square equations\n");
     printf("Enter a,b,c through a space (probel)\n");
 
